5b436710b4c239c565e35a8ce345ee1e216930ab
krzysiek1421 <krzysiek1421@gmail.com>
2023-04-14 22:57:01 +0200

    BASE: Init of the project

---
+++ src/main/java/com/krzysiek1421/eventplanner/EventPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.krzysiek1421.eventplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlannerApplication.class, args);
+	}
+
+}
